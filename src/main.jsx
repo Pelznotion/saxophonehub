76cf3b9b30d95c37913beaaad07d87f88054cb2d
Adeniyi Adejare <dejarepelumi5@gmail.com>
2025-10-27 11:45:51 -1100

    Fix build path for GitHub Pages1

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* 👇 Use basename to tell React Router your repo name */}
-    <BrowserRouter basename="/saxophonehub">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
