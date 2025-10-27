2fd2f55e15f115fb158646161f4abbde546eae2c
Adeniyi Adejare <dejarepelumi5@gmail.com>
2025-10-27 09:22:14 -1100

    Fix: GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import postcss from "postcss";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/saxophonehub/", // 👈 This line tells Vite where your app is hosted
+  base: "/saxophonehub/", // 👈 this fixes the blank page issue
 });
