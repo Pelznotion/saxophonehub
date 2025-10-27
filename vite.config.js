287d20f60a9bfa2963b2fc4d1ebff4136f4d820c
Adeniyi Adejare <dejarepelumi5@gmail.com>
2025-10-27 09:17:35 -1100

    Fix: added base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,5 @@ import postcss from "postcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/saxophonehub/", // 👈 This line tells Vite where your app is hosted
 });
